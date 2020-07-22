53f8f6bcd3864f9478de2e1f3d8c987c195611f4
Alpha-1729 <anilakskumar1729@gmail.com>
2020-07-22 22:23:32 +0530

    day5 content added

---
+++ day-5/my_function.py
@@ -0,0 +1,16 @@
+# my_functions.py -> This is the name of this file.
+def sayHello():
+    return "Hello Sir"
+def cube(num):
+    return num*num*num
+def root(number):
+    return number**0.5
+
+print(sayHello())
+
+print("This is my program.")
+print("Dont copy my function.")
+print("you can import it.")
+
+print(cube(3))
+print(root(2))
